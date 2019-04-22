598757e85cb50c6e7da3b4f5b8c965876569f99e
patrozada <patriciarozada@gmail.com>
2019-04-22 19:59:19 +0200

    Fix fallback image logic

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
-//Get elements from HTML and save them in const
 const inputEl = document.getElementById('show__title');
 const buttonEl = document.querySelector('.btn');
 const listEl = document.querySelector('.search__output');
+const itemArr =[];
 
 function handleButtonClick(e){
   e.preventDefault();
@@ -12,9 +12,9 @@ function handleButtonClick(e){
     .then(response=>response.json())
     .then(showsArr=>{
       for(const show of showsArr){
-        const imgSrcMedium = `${show.show.image.medium}`
+        const imgObj =`${show.show.image}`;
         const imgSrcPh = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
-        const showName =`${show.show.name}`
+        const showName =`${show.show.name}`;
         const newItem = document.createElement('li');
         const newP = document.createElement('p');
         const newContent = document.createTextNode(showName);
@@ -24,14 +24,32 @@ function handleButtonClick(e){
         newItem.appendChild(newImg);
         listEl.appendChild(newItem);
         newImg.setAttribute('alt', showName +' image');
-        if(imgSrcMedium){
-          newImg.setAttribute('src', imgSrcMedium);
+        if(imgObj === 'null'){
+          newImg.setAttribute('src', imgSrcPh);
         }else{
-          newImg.setAttribute(imgSrcPh);
+          const imgSrcMedium = `${show.show.image.medium}`;
+          newImg.setAttribute('src', imgSrcMedium);
         }
+        itemArr.push(newItem);
       }
     })
     .catch(error=> console.error(`Ha sucedido un error: ${error}`));
 }
-
+console.log(itemArr);
 buttonEl.addEventListener('click', handleButtonClick);
+
+//Save each li in an array
+
+//add a click listener to each li
+for(let li of itemArr){
+  li.addEventListener('click', handleLiClick);
+}
+function handleLiClick(){
+  const wishlistEl = document.createElement('ul');
+  const favouritesEl = document.querySelector('.favourites__side');
+  favouritesEl.appendChild(wishlistEl);
+
+}
+// declare handler:
+  //on click: swap background and text colors
+  //on click: add li element to wishlist
