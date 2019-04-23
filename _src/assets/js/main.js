a901f88563761a447d35f36a2b64e73c43ff037b
patrozada <patriciarozada@gmail.com>
2019-04-23 11:59:42 +0200

    Create displayResponse function

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -3,53 +3,49 @@
 const inputEl = document.getElementById('show__title');
 const buttonEl = document.querySelector('.btn');
 const listEl = document.querySelector('.search__output');
-const itemArr =[];
+
+const displayResponse = function(showsArr){
+  for(const show of showsArr){    
+    const itemArr =[];
+    const imgObj =`${show.show.image}`;
+    const imgSrcPh = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+    const showName =`${show.show.name}`;
+    const newItem = document.createElement('li');
+    const newP = document.createElement('p');
+    const newContent = document.createTextNode(showName);
+    const newImg = document.createElement('img');
+    newP.appendChild(newContent);
+    newItem.appendChild(newP);
+    newItem.appendChild(newImg);
+    listEl.appendChild(newItem);
+    newImg.setAttribute('alt', showName +' image');
+    if(imgObj === 'null'){
+      newImg.setAttribute('src', imgSrcPh);
+    }else{
+      const imgSrcMedium = `${show.show.image.medium}`;
+      newImg.setAttribute('src', imgSrcMedium);
+    }
+    itemArr.push(newItem);
+    console.log(itemArr);
+  }
+};
 
 function handleButtonClick(e){
   e.preventDefault();
   const inputValue = inputEl.value;
   fetch(`http://api.tvmaze.com/search/shows?q=${inputValue}`)
     .then(response=>response.json())
-    .then(showsArr=>{
-      for(const show of showsArr){
-        const imgObj =`${show.show.image}`;
-        const imgSrcPh = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
-        const showName =`${show.show.name}`;
-        const newItem = document.createElement('li');
-        const newP = document.createElement('p');
-        const newContent = document.createTextNode(showName);
-        const newImg = document.createElement('img');
-        newP.appendChild(newContent);
-        newItem.appendChild(newP);
-        newItem.appendChild(newImg);
-        listEl.appendChild(newItem);
-        newImg.setAttribute('alt', showName +' image');
-        if(imgObj === 'null'){
-          newImg.setAttribute('src', imgSrcPh);
-        }else{
-          const imgSrcMedium = `${show.show.image.medium}`;
-          newImg.setAttribute('src', imgSrcMedium);
-        }
-        itemArr.push(newItem);
-      }
-    })
+    .then(displayResponse)
     .catch(error=> console.error(`Ha sucedido un error: ${error}`));
+
+  // for(const li of itemArr){
+  //   li.addEventListener('click', handleLiClick);
+  // }
+  // function handleLiClick(){
+  //   console.log();
+  //   const wishlistEl = document.createElement('ul');
+  //   const favouritesEl = document.querySelector('.favourites__side');
+  //   favouritesEl.appendChild(wishlistEl);
+  // }
 }
-console.log(itemArr);
 buttonEl.addEventListener('click', handleButtonClick);
-
-//Save each li in an array
-
-//add a click listener to each li
-for(let li of itemArr){
-  li.addEventListener('click', handleLiClick);
-}
-function handleLiClick(){
-  const wishlistEl = document.createElement('ul');
-  const favouritesEl = document.querySelector('.favourites__side');
-  favouritesEl.appendChild(wishlistEl);
-
-}
-// declare handler:
-  //on click: swap background and text colors
-  //on click: add li element to wishlist
